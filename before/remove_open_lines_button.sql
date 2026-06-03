f9e1e98f660234fed2709c4f212815294853025f
Xavi Hermoso <xhermoso@electrans.es>
2026-06-03 12:43:30 +0200

    Make remove open lines SQL work with versions lower than 7.2

--- before/remove_open_lines_button.sql
+++ before/remove_open_lines_button.sql
@@ -1,1 +1,1 @@
-DELETE FROM "ir_model_button" WHERE "name" = 'open_lines' AND "model" IN ('product.price_list', (SELECT "id"::text FROM "ir_model" WHERE "model" = 'product.price_list'));
+DELETE FROM "ir_model_button" WHERE "name" = 'open_lines' AND "model"::text IN ('product.price_list', (SELECT "id"::text FROM "ir_model" WHERE "model" = 'product.price_list'));
